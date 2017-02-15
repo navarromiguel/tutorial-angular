944d05d9baeb8ee5c6437bce7dffd3f165399f0b
mikelote <migue93@gmail.com>
2017-02-16 00:42:26 +0100

    paso2

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
 var app = angular.module('gemStore', []);
 app.controller('StoreController', function($scope){
-	var gem = { name: 'Diamante', price: 300 };
-	$scope.product = gem;
+	$scope.gems = [
+	  {
+	    "name": "Zafiro",
+	    "description": "El zafiro es un mineral precioso del grupo del corindón con característico color azul",
+	    "shine": 8,
+	    "price": 275,
+	    "rarity": 7,
+	    "color": "Azul",
+	    "faces": 14,
+	    "images": [
+	      "images/gem-02.gif",
+	      "images/gem-05.gif",
+	      "images/gem-09.gif"
+	    ]
+	  },
+	  {
+	    "name": "Esmeralda",
+	    "description": "La esmeralda es la variedad verde del berilo, un mineral ciclosilicato de berilio y aluminio.",
+	    "shine": 9,
+	    "price": 250,
+	    "rarity": 6,
+	    "color": "Verde",
+	    "faces": 12,
+	    "images": [
+	      "images/gem-01.gif",
+	      "images/gem-03.gif",
+	      "images/gem-04.gif"
+	    ]
+	  },
+	  {
+	    "name": "Diamante",
+	    "description": "El diamante es un alótropo del carbono donde los átomos de carbono están dispuestos en una variante de la estructura cristalina cúbica",
+	    "shine": 70,
+	    "price": 300,
+	    "rarity": 2,
+	    "color": "Transparente",
+	    "faces": 6,
+	    "images": [
+	      "images/gem-06.gif",
+	      "images/gem-07.gif",
+	      "images/gem-08.gif"
+	    ]
+	  }
+	];
+
 });
